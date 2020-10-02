1abd44fd17511b52f1886b1a22bc7e96dbdffaa4
Rachel Kang <rachel.j.kang@gmail.com>
2020-10-02 11:37:41 -0400

    Update RecipeSearchPage
    - Fix button interaction/logic
    - Get and render search results from API
    - Basic styling

--- WhatToEat/Views/RecipeSearchPage.xaml.cs
+++ WhatToEat/Views/RecipeSearchPage.xaml.cs
@@ -9,13 +9,16 @@ namespace Recipes.Views
     public partial class RecipeSearchPage : ContentPage
     {
         RestService _restService;
-        ItemsViewModel _viewModel;
 
         public RecipeSearchPage()
         {
             InitializeComponent();
-            BindingContext = _viewModel = new ItemsViewModel();
             _restService = new RestService();
+            ButtonText = "Find Recipe";
+            SearchQuery = "Macaroni";
+            BindingContext = this;
+
+            ItemTapped = new Command<Hit>(OnItemSelected);
         }
 
         async void OnButtonClicked(object sender, EventArgs e)
@@ -23,9 +26,36 @@ namespace Recipes.Views
             if (!string.IsNullOrWhiteSpace(searchEntry.Text))
             {
                 RecipeData recipeData = await _restService.GetRecipeDataAsync(GenerateRequestUri(Constants.EdamamEndpoint));
-                BindingContext = recipeData;
+
+                if (recipeData.Hits.Length == 0 || recipeData == null)
+                {
+                    NoResultsLabel.Text = $"Sorry - we couldn't find any recipes for {searchEntry.Text} :(";
+                    NoResultsLabel.IsVisible = true;
+                }
+                else
+                {
+                    NoResultsLabel.IsVisible = false;
+                }
+
+                for (int i = 0; i < recipeData.Hits.Length; i++)
+                {
+                    recipeData.Hits[i].Id = i;
+                }
+
+                RecipeData = recipeData;
+                AppShell.Data = RecipeData;
+                OnPropertyChanged(nameof(RecipeData)); // tells Xaml view to update
+                //BindingContext = recipeData;
             }
         }
+
+        public RecipeData RecipeData { get; set; }
+
+        public string ButtonText { get; set; }
+
+        public string SearchQuery { get; set; }
+
+        public string NoResults { get; set; }
 
         string GenerateRequestUri(string endpoint)
         {
@@ -35,6 +65,17 @@ namespace Recipes.Views
             requestUri += $"&app_key={Constants.EdamamAppKey}";
             //requestUri += $"&from=0&to=1";
             return requestUri;
+        }
+
+        public Command<Hit> ItemTapped { get; }
+
+        async void OnItemSelected(Hit hit)
+        {
+            if (hit == null)
+                return;
+
+            // This will push the ItemDetailPage onto the navigation stack
+            await Shell.Current.GoToAsync($"{nameof(HitDetailPage)}?HitId={hit.Id}");
         }
     }
 }
