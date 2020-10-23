ed059dd547d77798033fb76ccb4e8fee1b07fca0
Rachel Kang <rachel.j.kang@gmail.com>
2020-10-23 17:44:28 -0400

    Clean up RecipeSearch V and VM

--- WhatToEat/ViewModels/RecipeSearchViewModel.cs
+++ WhatToEat/ViewModels/RecipeSearchViewModel.cs
@@ -1,11 +1,96 @@
 ﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Recipes.Views;
+using Xamarin.Forms;
+
 namespace Recipes.ViewModels
 {
     public class RecipeSearchViewModel : BaseViewModel
     {
+        RestService _restService;
+
+        public Command<Hit> ItemTapped { get; }
+        public Command SearchCommand { get; }
+
         public RecipeSearchViewModel()
         {
-            Title = "Recipes";
+            Title = "Search Recipes";
+            _restService = new RestService();
+            ButtonText = "Search";
+            SearchQuery = "Macaroni";
+            //NoResultsLabel = $"Sorry - we couldn't find any recipes :(";
+
+            ItemTapped = new Command<Hit>(OnItemSelected);
+            SearchCommand = new Command(async () => await OnSearch());
+
         }
+
+        public RecipeData RecipeData { get; set; }
+
+        public string ButtonText { get; set; }
+
+        public string SearchQuery { get; set; }
+
+        public string NoResultsLabel { get; set; }
+
+        async Task OnSearch()
+        {
+            if (!string.IsNullOrWhiteSpace(SearchQuery))
+            {
+                RecipeData recipeData = await _restService.GetRecipeDataAsync(GenerateRequestUri(Constants.EdamamEndpoint));
+
+                if (recipeData.Hits.Length == 0 || recipeData == null)
+                {
+                    //NoResultsLabel = $"Sorry - we couldn't find any recipes for {SearchQuery} :(";
+                    //noResultsLabel.IsVisible = true;
+                }
+                else
+                {
+                    //noResultsLabel.IsVisible = false;
+                }
+
+                for (int i = 0; i < recipeData.Hits.Length; i++)
+                {
+                    recipeData.Hits[i].Id = i;
+                }
+
+                RecipeData = recipeData;
+                AppShell.Data = RecipeData;
+                OnPropertyChanged(nameof(RecipeData)); // tells Xaml view to update
+            }
+        }
+
+        string GenerateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?q={SearchQuery}";
+            requestUri += $"&app_id={Constants.EdamamAppId}";
+            requestUri += $"&app_key={Constants.EdamamAppKey}";
+            //requestUri += $"&from=0&to=1";
+            return requestUri;
+        }
+
+        async void OnItemSelected(Hit hit)
+        {
+            if (hit == null)
+                return;
+
+            // This will push the HitDetailPage onto the navigation stack
+            await Shell.Current.GoToAsync($"{nameof(HitDetailPage)}?HitId={hit.Id}");
+        }
+
+        //public class InverseBoolConverter : IValueConverter
+        //{
+        //    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        //    {
+        //        return !((bool)value);
+        //    }
+
+        //    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        //    {
+        //        return value;
+        //    }
+        //}
     }
 }
--- WhatToEat/Views/RecipeSearchPage.xaml.cs
+++ WhatToEat/Views/RecipeSearchPage.xaml.cs
@@ -8,74 +8,13 @@ namespace Recipes.Views
 {
     public partial class RecipeSearchPage : ContentPage
     {
-        RestService _restService;
+        RecipeSearchViewModel _viewModel;
 
         public RecipeSearchPage()
         {
             InitializeComponent();
-            _restService = new RestService();
-            ButtonText = "Find Recipe";
-            SearchQuery = "Macaroni";
-            BindingContext = this;
-
-            ItemTapped = new Command<Hit>(OnItemSelected);
+            BindingContext = _viewModel = new RecipeSearchViewModel();
         }
 
-        async void OnButtonClicked(object sender, EventArgs e)
-        {
-            if (!string.IsNullOrWhiteSpace(searchEntry.Text))
-            {
-                RecipeData recipeData = await _restService.GetRecipeDataAsync(GenerateRequestUri(Constants.EdamamEndpoint));
-
-                if (recipeData.Hits.Length == 0 || recipeData == null)
-                {
-                    NoResultsLabel.Text = $"Sorry - we couldn't find any recipes for {searchEntry.Text} :(";
-                    NoResultsLabel.IsVisible = true;
-                }
-                else
-                {
-                    NoResultsLabel.IsVisible = false;
-                }
-
-                for (int i = 0; i < recipeData.Hits.Length; i++)
-                {
-                    recipeData.Hits[i].Id = i;
-                }
-
-                RecipeData = recipeData;
-                AppShell.Data = RecipeData;
-                OnPropertyChanged(nameof(RecipeData)); // tells Xaml view to update
-                //BindingContext = recipeData;
-            }
-        }
-
-        public RecipeData RecipeData { get; set; }
-
-        public string ButtonText { get; set; }
-
-        public string SearchQuery { get; set; }
-
-        public string NoResults { get; set; }
-
-        string GenerateRequestUri(string endpoint)
-        {
-            string requestUri = endpoint;
-            requestUri += $"?q={searchEntry.Text}";
-            requestUri += $"&app_id={Constants.EdamamAppId}";
-            requestUri += $"&app_key={Constants.EdamamAppKey}";
-            //requestUri += $"&from=0&to=1";
-            return requestUri;
-        }
-
-        public Command<Hit> ItemTapped { get; }
-
-        async void OnItemSelected(Hit hit)
-        {
-            if (hit == null)
-                return;
-
-            // This will push the ItemDetailPage onto the navigation stack
-            await Shell.Current.GoToAsync($"{nameof(HitDetailPage)}?HitId={hit.Id}");
-        }
     }
 }
