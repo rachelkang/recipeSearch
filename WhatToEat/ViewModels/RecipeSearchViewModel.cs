d378deb779db5829ca2377129891102786b52dcd
Rachel Kang <rachel.j.kang@gmail.com>
2020-11-09 10:44:12 -0500

    Start update of RecipeSearchPage to take from StartingPage

--- WhatToEat/ViewModels/RecipeSearchViewModel.cs
+++ WhatToEat/ViewModels/RecipeSearchViewModel.cs
@@ -6,58 +6,88 @@ using Xamarin.Forms;
 
 namespace Recipes.ViewModels
 {
+    [QueryProperty(nameof(SearchQuery), nameof(SearchQuery))]
+    [QueryProperty(nameof(DietSearchFilter), nameof(DietSearchFilter))]
+    [QueryProperty(nameof(HealthSearchFilter), nameof(HealthSearchFilter))]
     public class RecipeSearchViewModel : BaseViewModel
     {
         RestService _restService;
 
+        private string searchQuery;
+        //private string dietSearchFilter;
+        //private string healthSearchFilter;
+        private string noResultsLabel;
+        private bool noResultsVisible;
         public Command<Hit> ItemTapped { get; }
         public Command SearchCommand { get; }
 
         public RecipeSearchViewModel()
         {
-            Title = "Search Recipes";
+            Title = "Search Results";
             _restService = new RestService();
-            ButtonText = "Search";
-            SearchQuery = "Macaroni";
-            //NoResultsLabel = $"Sorry - we couldn't find any recipes :(";
+            NoResultsVisible = false;
 
             ItemTapped = new Command<Hit>(OnItemSelected);
             SearchCommand = new Command(async () => await OnSearch());
-
         }
 
         public RecipeData RecipeData { get; set; }
 
-        public string ButtonText { get; set; }
+        public string SearchQuery
+        {
+            get
+            {
+                return searchQuery;
+            }
+            set
+            {
+                searchQuery = value;
+                _ = OnSearch();
+            }
+        }
 
-        public string SearchQuery { get; set; }
+        public string DietSearchFilter { get; set; }
 
-        public string NoResultsLabel { get; set; }
+        public string HealthSearchFilter { get; set; }
+
+        public string NoResultsLabel
+        {
+            get => noResultsLabel;
+            set => SetProperty(ref noResultsLabel, value);
+        }
+
+        public bool NoResultsVisible
+        {
+            get => noResultsVisible;
+            set => SetProperty(ref noResultsVisible, value);
+        }
 
         async Task OnSearch()
         {
+            NoResultsVisible = false;
+
             if (!string.IsNullOrWhiteSpace(SearchQuery))
             {
                 RecipeData recipeData = await _restService.GetRecipeDataAsync(GenerateRequestUri(Constants.EdamamEndpoint));
 
-                if (recipeData.Hits.Length == 0 || recipeData == null)
+                if (recipeData == null || recipeData.Hits.Length == 0)
                 {
-                    //NoResultsLabel = $"Sorry - we couldn't find any recipes for {SearchQuery} :(";
-                    //noResultsLabel.IsVisible = true;
+                    NoResultsLabel = $"Sorry - we couldn't find any recipes for {SearchQuery} :(";
+                    NoResultsVisible = true;
                 }
                 else
                 {
-                    //noResultsLabel.IsVisible = false;
-                }
+                    NoResultsVisible = false;
 
-                for (int i = 0; i < recipeData.Hits.Length; i++)
-                {
-                    recipeData.Hits[i].Id = i;
-                }
+                    for (int i = 0; i < recipeData.Hits.Length; i++)
+                    {
+                        recipeData.Hits[i].Id = i;
+                    }
 
-                RecipeData = recipeData;
-                AppShell.Data = RecipeData;
-                OnPropertyChanged(nameof(RecipeData)); // tells Xaml view to update
+                    RecipeData = recipeData;
+                    AppShell.Data = RecipeData;
+                    OnPropertyChanged(nameof(RecipeData)); // tells Xaml view to update
+                }
             }
         }
 
@@ -67,6 +97,8 @@ namespace Recipes.ViewModels
             requestUri += $"?q={SearchQuery}";
             requestUri += $"&app_id={Constants.EdamamAppId}";
             requestUri += $"&app_key={Constants.EdamamAppKey}";
+            //requestUri += $"&diet={DietSearchFilter}";
+            //requestUri += $"&health={HealthSearchFilter}";
             //requestUri += $"&from=0&to=1";
             return requestUri;
         }
@@ -79,18 +111,5 @@ namespace Recipes.ViewModels
             // This will push the HitDetailPage onto the navigation stack
             await Shell.Current.GoToAsync($"{nameof(HitDetailPage)}?HitId={hit.Id}");
         }
-
-        //public class InverseBoolConverter : IValueConverter
-        //{
-        //    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        //    {
-        //        return !((bool)value);
-        //    }
-
-        //    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        //    {
-        //        return value;
-        //    }
-        //}
     }
 }
