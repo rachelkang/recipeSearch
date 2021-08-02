eb6f96115c8bb4e79be7115cdbbae8373ec4397a
Shane Neuville <shneuvil@microsoft.com>
2021-08-02 15:06:57 -0500

    - fixup iOS to read entire layout

---
+++ WhatToEat.iOS/CustomFrameRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Foundation;
+using Recipes;
+using Recipes.iOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(CustomFrame), typeof(CustomFrameRenderer))]
+namespace Recipes.iOS
+{
+    public class CustomFrameRenderer : FrameRenderer
+    {
+        protected override async void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        {
+            base.OnElementChanged(e);
+
+            NativeView.AccessibilityTraits |= UIAccessibilityTrait.Button;
+
+            var things = NativeView.AccessibilityLabel;
+            await Task.Delay(4000);
+            things = NativeView.AccessibilityLabel;
+        }
+    }
+}
