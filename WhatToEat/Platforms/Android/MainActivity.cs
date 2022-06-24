e5d44aa00a5308cd730015c7a7eac1dcdbc65a7d
Shane Neuville <shneuvil@microsoft.com>
2022-06-24 10:38:52 -0500

    Fix platform heads

--- WhatToEat/Platforms/Android/MainActivity.cs
+++ WhatToEat/Platforms/Android/MainActivity.cs
@@ -1,28 +1,12 @@
 ﻿using Android.App;
+using Android.Content.PM;
 using Microsoft.Maui;
 using Microsoft.Maui.ApplicationModel;
 
 namespace Recipes
 {
-	[IntentFilter(
-		new[] { Platform.Intent.ActionAppAction },
-		Categories = new[] { Android.Content.Intent.CategoryDefault })]
-	[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true)]
-	public class MainActivity : MauiAppCompatActivity
-	{
-
-		protected override void OnResume()
-		{
-			base.OnResume();
-
-			Platform.OnResume(this);
-		}
-
-		protected override void OnNewIntent(Android.Content.Intent intent)
-		{
-			base.OnNewIntent(intent);
-
-			Platform.OnNewIntent(intent);
-		}
-	}
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+    }
 }
