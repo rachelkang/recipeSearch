a9ad6803d230796d284772ad8f589447e9ed1551
Shane Neuville <shneuvil@microsoft.com>
2022-08-30 15:21:01 -0500

    Set accessibility traits

--- WhatToEat/Views/ItemDetailPage.xaml.cs
+++ WhatToEat/Views/ItemDetailPage.xaml.cs
@@ -22,8 +22,21 @@ namespace Recipes.Views
 
 		async void OpenUrl(object sender, System.EventArgs e)
 		{
+            System.Diagnostics.Debug.WriteLine("WHAT");
 			await Launcher.OpenAsync(_viewModel.RecipeUrl);
 
 		}
-	}
+
+        void Button_Loaded(System.Object sender, System.EventArgs _)
+        {
+#if IOS || MACCATALYST
+            if (sender is IElement e && e.Handler.PlatformView is UIKit.UIView uiView)
+            {
+                uiView.AccessibilityTraits = UIKit.UIAccessibilityTrait.Link;
+                System.Diagnostics.Debug.WriteLine($"{uiView.AccessibilityTraits}");
+            }
+            System.Diagnostics.Debug.WriteLine("asdf");
+#endif
+        }
+    }
 }
